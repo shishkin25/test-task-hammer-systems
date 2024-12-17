55026ed7ff034793f6f759bee44aeb399de63793
Artem Shishkin <a.shishkin02@gmail.com>
2024-12-17 16:16:36 +0300

    Updates

--- static/js/17.021e3ff6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonptest-task-hammer-systems"]=this["webpackJsonptest-task-hammer-systems"]||[]).push([[17],{492:function(s,t,e){"use strict";e.r(t);var n=e(2);e(0);t.default=function(){return Object(n.jsx)("div",{children:"Home component works!"})}}}]);
-//# sourceMappingURL=17.021e3ff6.chunk.js.map
---
+++ static/js/17.3c808381.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[17],{492:function(e,s,n){"use strict";n.r(s);var t=n(2);n(0);s.default=function(){return Object(t.jsx)("div",{children:"Home component works!"})}}}]);
+//# sourceMappingURL=17.3c808381.chunk.js.map
